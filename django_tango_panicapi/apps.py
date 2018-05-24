bb69e218a078b91f12b736e316272f47cd4364b8
Piotr Goryl <piotr.goryl@s2innovation.com>
2018-05-24 15:44:27 +0200

    First import of the application

---
+++ django_tango_panicapi/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class DjangoTangoPanicapiConfig(AppConfig):
+    name = 'django_tango_panicapi'
